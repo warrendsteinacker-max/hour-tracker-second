31e971edbab4a437a74a662dfbfc211c03897fa4
Warren <warrendsteinacker@gmail.com>
2026-02-17 09:45:05 -0500

    testing img and css

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -7,8 +7,10 @@ function App() {
 
   return (
     <>
-      <div style={{position: 'absolute', top: '20%', left: '20%', display: 'flex', flex-direction: 'row' }}>
-        <img src='vite.svg' />
+      <div style={{position: 'absolute', top: '2%', left: '5%', display: 'flex', flexDirection: 'row', background: 'linear-gradient(to left, purple, red)', borderRadius: '15px', padding: '20px', gap: '20px', width: '90%'}}>
+        <img src='vite.svg' style={{border: '5px solid white'}}/>
+        <button style={{padding: '10px'}}>make post</button>
+        <h3>Welcom to my Blog</h3>
       </div>
     </>
   )
